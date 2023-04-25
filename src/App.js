02a7693ca236f4c1549ea627406795b2c0e11ba7
Jeedivamsi <jeedivamsikrishna@gmail.com>
2023-04-25 15:19:18 +0530

    Todo list is created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useRef} from 'react';
 
 function App() {
+  const [data, setData] = useState("")
+  const [list, setList] = useState([])
+  const inputEle = useRef();
+
+  const handleChange=(e)=>{
+      setData(e.target.value)
+  }
+  const handleSubmit=(e)=>{
+      // e.preventDefault();
+      inputEle.current.focus();
+        const res = [...list, data]
+        setList(res)
+        setData("")
+  }
+  const handleDelete=(i)=>{
+          const result = list.filter((item,index)=>
+          i!==index)
+          setList(result);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <input ref={inputEle} type='text' placeholder="enter some text..." value={data} onChange={handleChange}/>
+    <button onClick={handleSubmit}>Add</button>
+    <hr/>
+    <ul>
+      {
+        list.length === 0 ? <h3>Todo_List is empty</h3> : list.map((item,index)=>
+        <li key={index}>{item}&nbsp;&nbsp;&nbsp;<button onClick={()=>handleDelete(index)}>Remove</button></li>)
+      }
+    </ul>
     </div>
   );
 }
